0f8ef8b2f62ab477a56658a12cd0052e2e2322ec
Prince Kumar <prince_kumar1@persistent.com>
2022-05-31 09:24:49 +0530

    condition if

---
+++ LoopsCondition.js
@@ -0,0 +1,3 @@
+let statusdata =200;
+const message = (statusdata === 200) ? 'ok' : 'not ok'; //Ternary operator
+console.log(message)
--- index.js
+++ index.js
@@ -54,3 +54,31 @@ console.log(num1--);
 console.log("__________Using math object_________");
 console.log(Math.PI);
 console.log(Math.sqrt(num1));
+console.log("__________Convert number to string and vise versa_________");
+let flo1 = '1.50';
+console.log(parseInt('100'));
+console.log(parseInt(num1));
+console.log(parseInt('ABC'));
+console.log(parseInt('OXF'));
+console.log(parseInt('0xF'));
+
+console.log("********Converting strings to float********");
+//Converting strings to float
+console.log(parseFloat('1.00'));
+console.log(parseFloat(flo1));
+console.log(parseFloat('ABC'));
+
+console.log("********More Conversion Examples********");
+//More Conversion Examples
+//Numbers after special characters are ignored
+console.log(parseInt('1.5')); 
+console.log(parseInt('1 + 1'));
+
+//Using Template Literals
+console.log(parseInt(`${1 + 1}`));
+
+console.log("********Converting numbers to strings********");
+//Converting numbers to strings
+console.log(num1.toString());
+console.log(flo1.toString());
+console.log((100).toString());
