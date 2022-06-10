c721ecf08304df982989d5f8c8e0b63883fe0134
Prince Kumar <prince.m.singh11@gmail.com>
2022-06-10 21:22:25 +0530

    link list add and remove

--- Data-Structure/LinkListDemo.js
+++ Data-Structure/LinkListDemo.js
@@ -1,13 +1,13 @@
-class Node{
-    constructor(element){
+class Node {
+    constructor(element) {
         this.element = element; //Store data value
         this.next = null; // Store refrence of next element
     }
 
 }
 
-class LinkedList{
-    constructor(){
+class LinkedList {
+    constructor() {
         this.head = null; // Store first node of list
         this.size = 0; // stroe size of list
     }
@@ -24,39 +24,41 @@ class LinkedList{
     ->PrintList
     --*/
 
-    add(eleement){
+    add(eleement) {
         // create new node
         var node = new Node(eleement);
         // to store current node
         var current;
         // check list is empty then add element and make head
-        if(this.head == null){
+        if (this.head == null) {
             this.head = node;
-        }else{
+        } else {
             current = this.head;
-            while(current.next){
+            while (current.next) {
                 current = current.next;
             }
             current.next = node;
         }
         this.size++;
     }
-    insertAt(eleement,index){
-        if(index <0 || index >this.size){
+
+    //Insert element at index
+    insertAt(eleement, index) {
+        if (index < 0 || index > this.size) {
             return console.log("Please enter a valid index");
-        }else{
+        } else {
             var node = new Node(eleement);
             var curr, prev;
             curr = this.head;
-            
+
             //Add at first index
-            if(index == 0){
+            if (index == 0) {
                 node.next = this.head;
                 this.head = node;
-            }else{
+            } else {
                 curr = this.head;
-                var it =0;
-                while(it <index){
+                var it = 0;
+                while (it < index) {
                     it++;
                     prev = curr;
                     curr = curr.next;
@@ -70,24 +72,72 @@ class LinkedList{
         }
     }
 
+    //Remove form index
+    removeFrom(index) {
+        if (index < 0 || index >= this.size) {
+            return console.log("Please enter a vaild index");
+
+        } else {
+            var curr, prev, it = 0;
+            curr = this.head;
+            prev = curr;
+
+            if (index === 0) {
+                this.head = curr.next;
+            } else {
+                while (it < index) {
+                    it++;
+                    prev = curr;
+                    curr = curr.next;
+                }
+
+                //remove element
+                prev.next = curr.next;
+            }
+            this.size--;
+            return curr.eleement;
+
+        }
+    }
+    removeElement(eleement) {
+        var current = this.head;
+        var prev = null;
+        while (current != null) {
+            //comparing element with current element if found then remove 
+            //then and return true
+            if (current.eleement === eleement) {
+                if (prev == null) {
+                    this.head = current.next;
+                } else {
+                    prev.next = current.next;
+                }
+                this.size--;
+                return current.eleement;
+            }
+            prev = current;
+            current = current.next;
+        }
+        return -1;
+    }
+
     //Helper function
     indexOf(element) {
-       var count = 0;
+        var count = 0;
         var current = this.head;
- 
+
         // iterate over the list
         while (current != null) {
             // compare each element of the list
             // with given element
             if (current.element === element)
-            return count;
+                return count;
             count++;
             current = current.next;
         }
- 
-      // not found
+
+        // not found
         return -1;
     }
-    
+
 
 }
