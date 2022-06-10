7977da03e8136d6780bc25a18e5e159c42e0548d
Prince Kumar <prince.m.singh11@gmail.com>
2022-06-10 08:14:47 +0530

    list helper function

--- Data-Structure/LinkListDemo.js
+++ Data-Structure/LinkListDemo.js
@@ -69,6 +69,25 @@ class LinkedList{
             this.size++;
         }
     }
+
+    //Helper function
+    indexOf(element) {
+       var count = 0;
+        var current = this.head;
+ 
+        // iterate over the list
+        while (current != null) {
+            // compare each element of the list
+            // with given element
+            if (current.element === element)
+            return count;
+            count++;
+            current = current.next;
+        }
+ 
+      // not found
+        return -1;
+    }
     
 
 }
